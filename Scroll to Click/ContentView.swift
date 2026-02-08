da8c57073295147be917757e8d792b5fd30e839b
Sergei Kozelko <kozelkos@yandex.ru>
2026-02-08 17:56:10 +0800

    Initial Commit

---
+++ Scroll to Click/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scroll to Click
+//
+//  Created by Sergei on 4/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scroll to Click/Scroll_to_ClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scroll_to_ClickApp.swift
+//  Scroll to Click
+//
+//  Created by Sergei on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Scroll_to_ClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
