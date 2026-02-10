13fe54f28e55a3076012fb695079dbf0ea0a7c26
Sergei Kozelko <kozelkos@yandex.ru>
2026-02-10 17:09:32 +0800

    Migrate menu to SwiftUI

--- Scroll to Click/AppDelegate.swift
+++ Scroll to Click/AppDelegate.swift
@@ -9,41 +9,10 @@ import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    private var statusItem: NSStatusItem!
     private let interceptor = ScrollToClick()
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-
         // Start intercepting immediately
         interceptor.start()
-
-        // Menu bar item
-        statusItem = NSStatusBar.system.statusItem(
-            withLength: NSStatusItem.squareLength
-        )
-
-        if let button = statusItem.button {
-            let image = NSImage(
-                systemSymbolName: "computermouse",
-                accessibilityDescription: "Scroll to Click"
-            )
-            image?.isTemplate = true
-            button.image = image
-        }
-
-        let menu = NSMenu()
-        menu.addItem(
-            NSMenuItem(
-                title: "Quit",
-                action: #selector(quit),
-                keyEquivalent: "q"
-            )
-        )
-
-        statusItem.menu = menu
-    }
-
-    @objc private func quit() {
-        NSApp.terminate(nil)
     }
 }
--- Scroll to Click/ScrollToClickApp.swift
+++ Scroll to Click/ScrollToClickApp.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 @main
 struct ScrollToClickApp: App {
-    
+
     @NSApplicationDelegateAdaptor(AppDelegate.self)
     var appDelegate
-    
+
     var body: some Scene {
+        MenuBarExtra {
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }
+            .keyboardShortcut("q")
+        } label: {
+            Image(systemName: "computermouse")
+                .accessibilityLabel("Scroll to Click")
+        }
+
         Settings {
             EmptyView()
         }
